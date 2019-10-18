f9ca0081c3bc6be4a781c7fd23582ca5d7625e18
ricardoc333 <ricardo_c3@hotmail.com>
2019-10-18 01:47:45 -0300

    Initial commit

---
+++ PoCNetClienteJava/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        public string respuestaServicio = "_no_hubo_respuesta_";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:8080/Java-Spring-Hello-World-Service/hello");
+                // Set some reasonable limits on resources used by this request
+                request.MaximumAutomaticRedirections = 4;
+                request.MaximumResponseHeadersLength = 4;
+                // Set credentials to use for this request.
+                request.Credentials = CredentialCache.DefaultCredentials;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                // Get the stream associated with the response.
+                Stream receiveStream = response.GetResponseStream();
+                // Pipes the stream to a higher level stream reader with the required encoding format. 
+                StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+                respuestaServicio = readStream.ReadToEnd();
+                response.Close();
+                readStream.Close();
+            } catch {}
+        }
+    }
+}
